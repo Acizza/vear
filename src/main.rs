67937d12e20708a213a3eee74349fa9af9c4138a
Acizza <jgit@tuta.io>
2020-11-19 21:39:11 -0800

    Replace gumdrop crate with argh
    The argh crate feels a bit more ergonomic, and reduces compile times a bit.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 #![allow(clippy::cast_precision_loss)]
 #![allow(clippy::shadow_unrelated)]
 #![allow(clippy::module_name_repetitions)]
-#![allow(clippy::default_trait_access)]
 
 mod archive;
 mod ui;
@@ -12,25 +11,23 @@ mod util;
 
 use anyhow::{anyhow, Context, Result};
 use archive::Archive;
-use gumdrop::Options;
-use std::path::PathBuf;
+use argh::FromArgs;
 use ui::{CycleResult, UI};
 
-#[derive(Debug, Options)]
-struct CmdArgs {
-    #[options(help = "print help message")]
-    help: bool,
-    #[options(free)]
-    path: Vec<String>,
+#[derive(FromArgs)]
+/// View, extract, and mount archives in the terminal.
+struct Args {
+    /// the path of the archive to open
+    #[argh(positional)]
+    path: String,
 }
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    let args = CmdArgs::parse_args_default_or_exit();
-    let path = PathBuf::from(args.path.join(" "));
+    let args: Args = argh::from_env();
 
-    let archive = Archive::read(&path)
-        .with_context(|| anyhow!("failed to read files from {}", path.display()))?;
+    let archive = Archive::read(&args.path)
+        .with_context(|| anyhow!("failed to read files from {}", args.path))?;
 
     let mut ui = UI::init(archive)?;
 
